6ea1ac07930a1d1fdb19799d3a72e66a80384a95
Vineetttt <vineetchotaliya30@gmail.com>
2024-10-30 21:23:36 +0530

    hiding deribit credentials

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include "DeribitTrading.h"
 #include "WebSocketServer.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
 
 int main() {
-    std::string client_id = "nl4jIiYR";
-    std::string client_secret = "-y7JN6FPCR7vkF864OJ-o78rpNxW8wn-g8D1q23ZoaE";
+    std::string client_id = std::getenv("DERIBIT_CLIENT_ID");
+    std::string client_secret = std::getenv("DERIBIT_CLIENT_SECRET");
     
     try {
         DeribitTrading trading(client_id, client_secret);
@@ -13,14 +16,36 @@ int main() {
             return 1;
         }
 
-        trading.placeOrder("BTC-PERPETUAL", 10, "limit", "market0000234", 2000.0);
+        auto start = std::chrono::high_resolution_clock::now();
+
+        std::vector<std::thread> threads;
+        for (int i = 0; i < 20; i++) {
+            threads.emplace_back([&trading, &i]() {
+                trading.placeOrder("SOL_USDC-PERPETUAL", 10, "limit", "market0000" + std::to_string(i), 2.0);
+            });
+        }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+        std::cout << "Time taken to place 20 orders: " << duration << " ms" << std::endl;
+
+        /*
+        trading.placeOrder("ETH_PERPETUAL", 10, "limit", "market000012", 2.0);
         trading.cancelOrder("29208640589");
         trading.modifyOrder(29213881074, 100);
         trading.getOrderBook("ETH-PERPETUAL", 1);
         trading.getPosition("ETH-PERPETUAL");
+        */
 
+        /*
         DeribitWebSocketServer server;
         server.start(9002);
+        */
 
     } catch (const std::exception& e) {
         Logger::getInstance()->log("Critical error: " + std::string(e.what()), "CRITICAL");
